c7bc7800cbf496372b12152ec807ad494410e705
Andrey Tropin <TropinAB@sibur.ru>
2025-11-04 18:11:39 +0300

    HomeWork10 add function GetDataType and test for it

--- HomeWork10/script.js
+++ HomeWork10/script.js
@@ -9,21 +9,24 @@ const patternEMail = /[\w\-\.]+@[\w\-]+\.[a-z]{2,5}/ig;
 const patternPhone = /\+\d{1}[-\s]?\d{3}[-\s]?\d{3}[-\s]?\d{4}/;
 const patternPhone2 = /\+\d{1}([\-\s]?\d){10}/;
 
-const input = prompt ('Введите дату/емайл/номер телефона');
-
-if (patternDate.test(input))
-    console.log(`${input} - дата`); 
-else if (patternEMail.test(input))
-    console.log(`${input} - е-майл`);
-else if (patternPhone.test(input))
-    console.log(`${input} - телефон`);
-else if (patternPhone2.test(input))
-    console.log(`${input} - телефон расширенный`);
-else
-    console.log(`${input} - не опознан`);
-
 //На разборе предложены шаблоны:
 const reg1 = /\d{1,2}[\.\/]\d{2}[\.\/]\d{2,4}/; //Дата
 const reg2 = /[\da-z\.]+@[\da-z]+\.[a-z]+/; //е-майл
 const reg3 = /\+\d[\s\(\-]{0,1}\d{3}[\s\)\-]{0,1}\d{3}[\s\-]{0,1}\d{2}[\s\-]{0,1}\d{2}/; //телефон
+
+function GetDataType(data) {
+    if (patternDate.test(input))
+        return 'дата'; 
+    else if (patternEMail.test(input))
+        return 'е-майл';
+    else if (patternPhone.test(input))
+        return 'телефон';
+    else if (patternPhone2.test(input))
+        return 'телефон расширенный';
+    else
+        return 'не известен';
+}
+
+const input = prompt ('Введите дату/емайл/номер телефона');
+console.log(`${input} - ${GetDataType(input)}`); 
 
