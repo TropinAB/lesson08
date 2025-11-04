47d1af28878b2e14dfdbbfa264cd66a99b8e6a0b
Andrey Tropin <TropinAB@sibur.ru>
2025-11-04 18:56:05 +0300

    HomeWork09 add functions and tests for it

--- HomeWork09/script.js
+++ HomeWork09/script.js
@@ -1,28 +1,33 @@
 // 1.Даны длины трёх сторон треугольника. Определить,
 // является ли треугольник прямоугольным.
-const [a, b, c] = [3,4,5].sort((a,b) => a - b);
-console.log(a,b,c);
-if ( a*a + b*b == c*c) 
-    console.log('Прямоугольный');
-else
-    console.log('НЕ прямоугольный');
+function isRightTriangle(a, b, c) {
+    [a, b, c] = [a, b, c].sort((a,b) => a - b);
+    return (a*a + b*b === c*c);
+}
+console.log(`${isRightTriangle(5,4,3) ? 'Прямоугольный' : 'НЕ прямоугольный'}`);
 
 // 2.Пользователь вводит число R. Написать программу,
 // которая выведет в консоль длину окружности и
 // площадь круга с радиусом R.
+function CircleLength(r) {
+    return r > 0 ? 2*Math.PI*r : 0;
+}
+function CircleArea(r) {
+    return r > 0 ? Math.PI*r*r : 0;
+}
 const R = +prompt('Введите R');
-console.log(`Длина окружности = ${(2*Math.PI*R).toFixed(3)}`);
-console.log(`Площадь круга = ${(Math.PI*R*R).toFixed(3)}`);
+console.log(`Длина окружности = ${CircleLength(R).toFixed(3)}`);
+console.log(`Площадь круга = ${CircleArea(R).toFixed(3)}`);
 
 // *Пользователь вводит числа a, b и c. Написать
 // программу, выводящую корни квадратного
 // уравнения с коэффициентами a, b и c.
-try {
-    const [a=0, b=0, c=0] = prompt('a b c:').split(' ');
+function SolveQuadraticEquation(a, b, c) {
     const D = b*b - 4*a*c;
     const x1 = ( -b + Math.sqrt(D) ) / (2 * a);
     const x2 = ( -b - Math.sqrt(D) ) / (2 * a);
-    console.log(x1, x2);
-} catch (error) {
-    console.error(error);
+    return [x1, x2];
 }
+
+const [a=0, b=0, c=0] = prompt('a b c:').split(' ');
+console.log(SolveQuadraticEquation(a, b, c));
