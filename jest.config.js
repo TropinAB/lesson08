6af50f26f85b9ecf7d22b0c213d014148435f673
Andrey Tropin <TropinAB@sibur.ru>
2025-11-09 15:14:00 +0300

    Fix jest.config for see subfolders

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,7 @@ const config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    'HomeWork**/*.{js,jsx}',
+    'HomeWork**/**/*.{js,jsx}',
     '!**/node_modules/**',
   ],
 
