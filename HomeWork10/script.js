1fc78b0f4c58347a5e5521b64de7004c59f719fd
Andrey Tropin <TropinAB@sibur.ru>
2025-11-09 15:49:22 +0300

    Fix lint errors for HW10

--- HomeWork10/script.js
+++ HomeWork10/script.js
@@ -4,15 +4,15 @@
 // 1.Датой.
 const patternDate = /\d{1,2}\.\d{1,2}\.\d{4}/;
 // 2.Адресом электронной почты.
-const patternEMail = /[\w\-\.]+@[\w\-]+\.[a-z]{2,5}/ig;
+const patternEMail = /[\w-.]+@[\w-]+\.[a-z]{2,5}/ig;
 // 3.*Номером телефона.
 const patternPhone = /\+\d{1}[-\s]?\d{3}[-\s]?\d{3}[-\s]?\d{4}/;
-const patternPhone2 = /\+\d{1}([\-\s]?\d){10}/;
+const patternPhone2 = /\+\d{1}([-\s]?\d){10}/;
 
 //На разборе предложены шаблоны:
-const reg1 = /\d{1,2}[\.\/]\d{2}[\.\/]\d{2,4}/; //Дата
-const reg2 = /[\da-z\.]+@[\da-z]+\.[a-z]+/; //е-майл
-const reg3 = /\+\d[\s\(\-]{0,1}\d{3}[\s\)\-]{0,1}\d{3}[\s\-]{0,1}\d{2}[\s\-]{0,1}\d{2}/; //телефон
+// const reg1 = /\d{1,2}[\.\/]\d{2}[\.\/]\d{2,4}/; //Дата
+// const reg2 = /[\da-z\.]+@[\da-z]+\.[a-z]+/; //е-майл
+// const reg3 = /\+\d[\s\(\-]{0,1}\d{3}[\s\)\-]{0,1}\d{3}[\s\-]{0,1}\d{2}[\s\-]{0,1}\d{2}/; //телефон
 
 export function GetDataType(data) {
     if (patternDate.test(data))
